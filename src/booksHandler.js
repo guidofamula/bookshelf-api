19fa751086ea41aa4859b2e7414b52b2b1921486
guidofamula <guidofamula29@gmail.com>
2022-08-07 13:58:05 +0700

    Build save book

---
+++ src/booksHandler.js
@@ -0,0 +1,91 @@
+const {nanoid} = require('nanoid');
+const books = require('./books');
+
+
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const forFinished = (finish) => {
+    if (pageCount !== readPage ) {
+      finish = true;
+    } else {
+      finish = false;
+    }
+    return forFinished;
+  };
+
+  if (name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  };
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = forFinished(finished);
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isArraySuccess = books.filter((book) => book.id === id).length > 0;
+  // const isNameFill = books.filter((name) => books.name === undefined);
+  // const isReadPageBig = books.filter((readPage) => books.readPage >= pageCount);
+
+  if (isArraySuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+module.exports = {
+  addBookHandler,
+};
---
+++ src/booksServer.js
@@ -0,0 +1,17 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./booksRoutes');
+
+const serverInit = async () => {
+  const setUpServer = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  setUpServer.route(routes);
+
+  await setUpServer.start();
+  console.log(`Server Bookshelf API telah running`);
+  console.log(`Pada ${setUpServer.info.uri}`);
+};
+
+serverInit();
